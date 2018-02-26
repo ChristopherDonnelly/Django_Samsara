5b43801fb0665d6515506ba5289f555d06e68c8d
Chris Donnelly <christopher.d.donnelly@gmail.com>
2018-02-26 15:10:10 -0800

    Added bootstrap and initial gameboard UI design

--- apps/game_board_app/views.py
+++ apps/game_board_app/views.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.shortcuts import render, HttpResponse, redirect 
 from django.utils.html import escape 
  
-def index(request): 
-    return render(request, "game_board_app/index.html") 
+def draw_board(request): 
+    return render(request, "game_board_app/board.html")
