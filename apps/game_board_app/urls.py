5892f7f90629505312fefe6a1c10d672a4cd3a9e
Kristin Dubrule <kristin.dubrule@gmail.com>
2018-02-27 11:02:29 -0800

    Added place_building; dropped play route

--- apps/game_board_app/urls.py
+++ apps/game_board_app/urls.py
@@ -3,9 +3,8 @@ from . import views
  
 urlpatterns = [ 
     url(r'^$', views.index),
-	url(r'^play/(\d+)$',views.play),
     url(r'^populate_board$',views.populate_board),
     url(r'^update_board$',views.update_board),
-    url(r'^draw_board/', views.draw_board),
-    url(r'^place_building/(?P<row>\d+)/(?P<square>\d+)/(?P<element>\w+)', views.place_building)
+    url(r'^draw_board/(\d+)$', views.draw_board),
+   	url(r'^place_building', views.place_building)
 ] 
--- apps/game_board_app/views.py
+++ apps/game_board_app/views.py
@@ -1,13 +1,19 @@
 from __future__ import unicode_literals 
- 
 from django.shortcuts import render, HttpResponse, redirect 
 from django.contrib import messages
 from django.utils.html import escape
-
 from .models import Game, Row, Square, Element, Entity, User
- 
-def draw_board(request):
-    return render(request, "game_board_app/board.html")
+import json
+from django.http import JsonResponse
+
+def draw_board(request,game_id):
+	context = {
+		"game": Game.objects.get(id=game_id),
+		"rows": Row.objects.filter(game_id=game_id),
+		"user": User.objects.filter(id=request.session['user_session'])[0]
+	}
+	request.session['game_id'] = game_id
+	return render(request, "game_board_app/board.html")
 	
 def index(request): 
 	context = {
@@ -16,15 +22,6 @@ def index(request):
 	}
 	return render(request, "game_board_app/index.html", context)
 
-def play(request,game_id):
-	context = {
-		"game": Game.objects.get(id=game_id),
-		"rows": Row.objects.filter(game_id=game_id),
-		"user": User.objects.filter(id=request.session['user_session'])[0]
-	}
-	request.session['game_id'] = game_id
-	return render(request, "game_board_app/game.html", context)
-
 # Create a new game
 def populate_board(request):
 	game = Game.objects.new_game(request.session['user_session'])['game']
@@ -32,7 +29,8 @@ def populate_board(request):
 		for square in row.squares.all():
 			if square.entity:
 				print (square.entity.kind, square.entity.element.name, square.entity.element.color)
-	return redirect('play/{}'.format(game.id))
+	request.session['player_number'] = 1
+	return redirect('draw_board/{}'.format(game.id))
 
 
 def update_board(request):
@@ -40,11 +38,12 @@ def update_board(request):
 		print (request.POST)
 	return redirect('/game_board_app')
 
-def place_building(request,row,square,element):
+def place_building(request):
 	game_id = request.session['game_id']
-	building = Entity.objects.place_building(request.session['user_session'],game_id,row,square,element)
+	body = json.loads(request.body.decode('utf-8'))
+	building = Entity.objects.place_building(request.session['user_session'],game_id,body['row'],body['column'],body['element'])
 
 	if building['errors']:
-		for tag, error in building['errors'].iteritems():
+		for tag, error in building['errors'].items():
 			messages.error(request, error, extra_tags=tag)
-	return redirect('/game_board/play/{}'.format(game_id))
+	return JsonResponse({"success":True})
