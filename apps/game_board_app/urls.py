8f35611783e0bfc61f627ef4f9b1fca923b80dfb
Kristin Dubrule <kristin.dubrule@gmail.com>
2018-02-26 16:51:26 -0800

    Added place_building, draw_board

--- apps/game_board_app/urls.py
+++ apps/game_board_app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
 	url(r'^play/(\d+)$',views.play),
     url(r'^populate_board$',views.populate_board),
     url(r'^update_board$',views.update_board),
+    url(r'^draw_board/', views.draw_board),
+    url(r'^place_building/(?P<row>\d+)/(?P<square>\d+)/(?P<element>\w+)', views.place_building)
 ] 
