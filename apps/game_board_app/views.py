ee0d913d877a8ae34b95f4476ec120d768461ebf
Kristin Dubrule <kristin.dubrule@gmail.com>
2018-02-26 16:52:39 -0800

    Added place_building, change model import for py3

--- apps/game_board_app/views.py
+++ apps/game_board_app/views.py
@@ -1,10 +1,12 @@
 from __future__ import unicode_literals 
  
 from django.shortcuts import render, HttpResponse, redirect 
+from django.contrib import messages
 from django.utils.html import escape
-from models import *
+
+from .models import Game, Row, Square, Element, Entity, User
  
-def draw_board(request): 
+def draw_board(request):
     return render(request, "game_board_app/board.html")
 	
 def index(request): 
@@ -19,8 +21,8 @@ def play(request,game_id):
 		"game": Game.objects.get(id=game_id),
 		"rows": Row.objects.filter(game_id=game_id),
 		"user": User.objects.filter(id=request.session['user_session'])[0]
-
 	}
+	request.session['game_id'] = game_id
 	return render(request, "game_board_app/game.html", context)
 
 # Create a new game
@@ -29,11 +31,20 @@ def populate_board(request):
 	for row in game.rows.all():
 		for square in row.squares.all():
 			if square.entity:
-				print square.entity.kind, square.entity.element.name, square.entity.element.color
+				print (square.entity.kind, square.entity.element.name, square.entity.element.color)
 	return redirect('play/{}'.format(game.id))
 
 
 def update_board(request):
 	if request.method == 'POST':
-		print request.POST
+		print (request.POST)
 	return redirect('/game_board_app')
+
+def place_building(request,row,square,element):
+	game_id = request.session['game_id']
+	building = Entity.objects.place_building(request.session['user_session'],game_id,row,square,element)
+
+	if building['errors']:
+		for tag, error in building['errors'].iteritems():
+			messages.error(request, error, extra_tags=tag)
+	return redirect('/game_board/play/{}'.format(game_id))
