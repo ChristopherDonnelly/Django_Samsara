184f423218932fefdfe32fd31e6b355f737c4d8b
Ashley Ho <ashley.demei.ho@gmail.com>
2018-03-02 10:17:49 -0800

    added responsiveness and styling to lobby

--- apps/lobby_app/views.py
+++ apps/lobby_app/views.py
@@ -2,17 +2,31 @@ from __future__ import unicode_literals
 from django.shortcuts import render, HttpResponse, redirect 
 from django.utils.html import escape 
 from ..game_board_app.models import *
+import copy
 
 def index(request): 
-	current_user = User.objects.filter(id=request.session['user_session'])[0]
-	current_player = Player.objects.filter(user=current_user)
-	curr_players_game = Game.objects.raw("SELECT * FROM game_board_app_game g JOIN game_board_app_player p ON g.id = p.game_id WHERE p.user_id={}".format(current_user.id))
-	# hosted_games = Game.objects.filter(host__in=current_player)
-	others_games = Game.objects.exclude(host__in=current_player) 
-	# players_in_game = Players.objects.game
 	all_games = Game.objects.all()
+
+	current_user = User.objects.filter(id=request.session['user_session'])[0]
+	all_games = Game.objects.all()
+	current_players = Player.objects.filter(user=current_user)
+	# query filters out only current player's games
+	curr_players_game = Game.objects.raw("SELECT * FROM game_board_app_game g JOIN game_board_app_player p ON g.id = p.game_id WHERE p.user_id={}".format(current_user.id))
+
 	
-	print ("*******")
+	# deepcopy makes a copy of all games, so our edits will not affect all game objs
+	others_games = copy.deepcopy(all_games)
+	# iterate through all of the current_players games
+	for player in current_players:
+		others_games = others_games.exclude(id=player.game_id)
+		print player.game_id
+		
+	# hosted_games = Game.objects.filter(host__in=current_players)
+	# others_games = Game.objects.exclude(host__in=current_players) 
+	'''
+	^ alternatively, we can use .difference for others_games
+	'''
+	
 	context = {
 		"all_games": all_games,
 		"user": User.objects.filter(id=request.session['user_session'])[0],
@@ -20,6 +34,27 @@ def index(request):
 		"others_games": others_games,
 	}
 	return render(request, "lobby_app/index.html", context)
+
+def show_game_info(request, game_id):
+	all_games = Game.objects.all()
+	current_user = User.objects.filter(id=request.session['user_session'])[0]
+	all_games = Game.objects.all()
+	current_players = Player.objects.filter(user=current_user)
+	curr_players_game = Game.objects.raw("SELECT * FROM game_board_app_game g JOIN game_board_app_player p ON g.id = p.game_id WHERE p.user_id={}".format(current_user.id))
+
+	others_games = copy.deepcopy(all_games)
+	for player in current_players:
+		others_games = others_games.exclude(id=player.game_id)
+		print player.game_id
+
+	context = {
+		"all_games": all_games,
+		"user": User.objects.filter(id=request.session['user_session'])[0],
+		"curr_players_game": curr_players_game,
+		"others_games": others_games,
+	}
+	
+	return render(request, "lobby_app/game_info.html", context)
 
 def delete_board(request,game_id):
 	# if logged-in player is same as player who created game => allow player to delete game
